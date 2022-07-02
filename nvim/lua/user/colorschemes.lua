bd5d186fde8cde1396a264e3f62429276d1d5aa0
lucky-vers <vermalucky2004@gmail.com>
2022-07-02 18:21:09 +0530

    Modify colorscheme

--- nvim/lua/user/colorschemes.lua
+++ nvim/lua/user/colorschemes.lua
@@ -1,12 +1,13 @@
-local colorscheme = "gruvbox"
+local colorscheme  = "gruvbox"
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 
 if not status_ok then
-
   vim.notify("ERROR: Colorscheme " .. colorscheme .. " not found.")
   return
-
 end
 
-vim.cmd[[hi SignColumn guibg=#282828]]
-vim.cmd[[hi Visual guifg=#ffffff2f guibg=#504945 gui=none]]
+vim.cmd[[
+  hi SignColumn guibg=#282828
+  hi Visual     guifg=#ffffff2f guibg=#504945 gui=none
+  hi Search     guifg=#504945   guibg=#fbf1c7
+]]
