403bc8491abed8a208e3cbeab5e86b367157c618
lucky-vers <vermalucky2004@gmail.com>
2022-07-02 22:49:59 +0530

    Changed guibg to NONE

--- nvim/lua/user/gitgutter.lua
+++ nvim/lua/user/gitgutter.lua
@@ -1,8 +1,8 @@
 vim.cmd[[
-  hi GitGutterAdd          guibg=#B8BB26
-  hi GitGutterChange       guibg=#FABD2F
-  hi GitGutterDelete       guibg=#FB4934
-  hi GitGutterChangeDelete guibg=#FABD2F
+  hi GitGutterAdd          guibg=NONE
+  hi GitGutterChange       guibg=NONE
+  hi GitGutterDelete       guibg=NONE
+  hi GitGutterChangeDelete guibg=NONE
 
   let g:gitgutter_sign_added    = '\ +'
   let g:gitgutter_sign_modified = '\ ~'
