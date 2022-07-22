4fdd7badd7fc1949478a944511dcaa1857655096
lucky-vers <vermalucky2004@gmail.com>
2022-07-22 18:04:29 +0530

    Add spaces to brackets

--- nvim/lua/user/lualine.lua
+++ nvim/lua/user/lualine.lua
@@ -2,28 +2,28 @@ require('lualine').setup {
   options = {
     icons_enabled        = true,
     theme                = 'gruvbox',
-    component_separators = {left = '', right = ''},
-    section_separators   = {left = '', right = ''},
+    component_separators = { left = '', right = '' },
+    section_separators   = { left = '', right = '' },
     disabled_filetypes   = {},
     always_divide_middle = true,
     globalstatus         = false,
   },
   sections = {
-    lualine_a = {'mode'},
-    lualine_b = {{'branch', icon = ''}, 'diff', 'diagnostics'},
-    lualine_c = {{'filename', symbols = {modified = ' ', readonly = ' ', unnamed = '[NO NAME]'}, path = 3}},
-    lualine_x = {'encoding'},
-    lualine_y = {'filesize', {'filetype', colored = false }},
-    lualine_z = {'progress', 'location'}
+    lualine_a = { 'mode' },
+    lualine_b = { { 'branch', icon = '' }, 'diff', 'diagnostics' },
+    lualine_c = { { 'filename', symbols = { modified = ' ', readonly = ' ', unnamed = '[NO NAME]' }, path = 3 } },
+    lualine_x = { 'encoding' },
+    lualine_y = { 'filesize', { 'filetype', colored = false  } },
+    lualine_z = { 'progress', 'location' }
   },
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = {{'filename', symbols = {modified = ' ', readonly = ' ', unnamed = '[NO NAME]', path = 3}}},
-    lualine_x = {'location'},
+    lualine_c = { { 'filename', symbols = { modified = ' ', readonly = ' ', unnamed = '[NO NAME]', path = 3 } } },
+    lualine_x = { 'location' },
     lualine_y = {},
     lualine_z = {}
   },
   tabline = {},
-  extensions = {'neo-tree'}
+  extensions = { 'neo-tree' }
 }
