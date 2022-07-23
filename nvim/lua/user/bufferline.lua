18a1ec74ff2f88af3722bd0de68b54cbfd4de522
lucky-vers <vermalucky2004@gmail.com>
2022-07-23 17:10:35 +0530

    Remove buffer icons

--- nvim/lua/user/bufferline.lua
+++ nvim/lua/user/bufferline.lua
@@ -21,40 +21,15 @@ require("bufferline").setup({
     tab_size                     = 21,
     diagnostics                  = false, -- | "nvim_lsp" | "coc",
     diagnostics_update_in_insert = false,
-    -- diagnostics_indicator = function(count, level, diagnostics_dict, context)
-    --   return "("..count..")"
-    -- end,
-    -- NOTE: this will be called a lot so don't do any heavy processing here
-    -- custom_filter = function(buf_number)
-    --   -- filter out filetypes you don't want to see
-    --   if vim.bo[buf_number].filetype ~= "<i-dont-want-to-see-this>" then
-    --     return true
-    --   end
-    --   -- filter out by buffer name
-    --   if vim.fn.bufname(buf_number) ~= "<buffer-name-I-dont-want>" then
-    --     return true
-    --   end
-    --   -- filter out based on arbitrary rules
-    --   -- e.g. filter out vim wiki buffer from tabline in your work repo
-    --   if vim.fn.getcwd() == "<work-repo>" and vim.bo[buf_number].filetype ~= "wiki" then
-    --     return true
-    --   end
-    -- end,
-    offsets = { { filetype = "neo-tree", text = "", padding = 1 } },
-    show_buffer_icons       = true,
+    offsets = { { filetype = "neo-tree", text = "", padding = 0 } },
+    show_buffer_icons       = false,
     show_buffer_close_icons = true,
     show_close_icon         = true,
     show_tab_indicators     = true,
-    persist_buffer_sort     = true, -- whether or not custom sorted buffers should persist
-    -- can also be a table containing 2 custom separators
-    -- [focused and unfocused]. eg: { '|', '|' }
-    separator_style         = "thin", -- | "thick" | "thin" | { 'any', 'any' },
+    persist_buffer_sort     = true,
+    separator_style         = "thin",
     enforce_regular_tabs    = true,
     always_show_bufferline  = true,
-    -- sort_by = 'id' | 'extension' | 'relative_directory' | 'directory' | 'tabs' | function(buffer_a, buffer_b)
-    --   -- add custom logic
-    --   return buffer_a.modified > buffer_b.modified
-    -- end
   },
   highlights = {
 
